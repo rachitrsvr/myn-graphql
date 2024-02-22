5a9b36f2b55e35c5543c9de35062cc8e8046bcb2
rajendra.kumar@rsvrtech.com <rajendra.kumar@rsvrtech.com>
2024-02-22 17:00:28 +0530

    Updated commands for AddUser via mediatr

--- myn-graphql-sample/Data/Requests/Commands/AddUserCommand.cs
+++ myn-graphql-sample/Data/Requests/Commands/AddUserCommand.cs
@@ -1,6 +1,14 @@
-﻿namespace myn_graphql_sample.Data.Requests.Commands
+﻿using myn_graphql_sample.Entities;
+
+namespace myn_graphql_sample.Data.Requests.Commands
 {
     public class AddUserCommand
     {
+        public AddUserCommand(User user)
+        {
+            User = user;
+        }
+
+        public User User { get; }
     }
 }
