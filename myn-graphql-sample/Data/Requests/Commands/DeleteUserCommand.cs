6ace99e340b120b49c80b3d9f7b66f10013f6c27
rajendra.kumar@rsvrtech.com <rajendra.kumar@rsvrtech.com>
2024-02-22 21:02:25 +0530

    request commands for UpdateUser and DeleteUser via mediatr - done

---
+++ myn-graphql-sample/Data/Requests/Commands/DeleteUserCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using myn_graphql_sample.Entities;
+
+namespace myn_graphql_sample.Data.Requests.Commands
+{
+    public class DeleteUserCommand : IRequest<bool>
+    {
+        public int Id { get; }
+
+        public DeleteUserCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
