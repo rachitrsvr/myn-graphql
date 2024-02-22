219c175fda78d3addb9739b8813d50e8031df499
rajendra.kumar@rsvrtech.com <rajendra.kumar@rsvrtech.com>
2024-02-22 15:20:42 +0530

    Updated with new changes- request for UserById via mediatr

--- myn-graphql-sample/Data/Requests/Queries/GetUserById.cs
+++ myn-graphql-sample/Data/Requests/Queries/GetUserById.cs
@@ -1,12 +1,16 @@
 ﻿using MediatR;
 using myn_graphql_sample.Entities;
+using System.Collections.Generic;
 
 namespace myn_graphql_sample.Data.Requests.Queries
 {
     public class GetUserById : IRequest<IEnumerable<User>>
     {
-        public int Id { get; set; }
+        public GetUserById(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; }
     }
 }
-
-
