0d211349ca9036bcefd31464813304a890d4b565
Rachit Shah <rachitshah@rsvrtech.com>
2024-03-07 15:00:58 +0530

    updates in docker compose and added connector(debezium working but issue in kafka listener)

--- src/cli/Commands/Kafka/KafkaConsumerMessagesCommand.cs
+++ src/cli/Commands/Kafka/KafkaConsumerMessagesCommand.cs
@@ -12,13 +12,27 @@ namespace Myn.GraphQL.Cli.Commands.Kafka
         {
             var config = new ConsumerConfig
             {
-                BootstrapServers = "localhost:9092",
+                BootstrapServers = "localhost:9094",
                 GroupId = "test-consumer-group",
                 AutoOffsetReset = AutoOffsetReset.Earliest
             };
-
+            using (var adminClient = new AdminClientBuilder(config).Build())
+            {
+                try
+                {
+                    var topicMetadata = adminClient.GetMetadata(TimeSpan.FromSeconds(5));
+                    foreach (var topic in topicMetadata.Topics)
+                    {
+                        Console.WriteLine($"Topic: {topic.Topic}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+            }
             using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
-            consumer.Subscribe("my-topic");
+            consumer.Subscribe("postgres.public.student");
 
             try
             {
